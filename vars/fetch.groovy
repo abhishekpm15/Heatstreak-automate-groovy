c7685565a13b04af1479448399f60a73ec950e51
ABHISHEK PM <pabhishekm@gmail.com>
2024-10-13 09:49:03 +0530

    Create fetch.groovy

---
+++ vars/fetch.groovy
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+def call() {
+  OUTPUT="$(curl http://localhost:3000/users)"
+  echo "You output : ${OUTPUT}"
+  echo "Processing users..."
+  USER_ARRAY=()
+  
+  # Traverse each object and extract `username` and `client_access_token`
+  echo "$OUTPUT" | jq -c '.[]' | while read -r user; do
+      USERNAME=$(echo "$user" | jq -r '.githubUserName')
+      CLIENT_TOKEN=$(echo "$user" | jq -r '.client_access_token')
+  
+      echo "Username: $USERNAME"
+      echo "Client Token: $CLIENT_TOKEN"
+      USER_ARRAY+=("{\"githubUserName\": \"$USERNAME\", \"client_access_token\": \"$CLIENT_TOKEN\"}")
+  done
+  
+  echo "Final array : $USER_ARRAY"
+}
